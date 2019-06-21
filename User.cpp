12c03a8ded69168d48fd9ec73abb87ad02bb0186
lukaszWerdymora <lukasz.jaworski.programista@gmail.com>
2019-06-21 21:37:47 +0200

    Almost Empty Project

---
+++ User.cpp
@@ -0,0 +1,17 @@
+#include "User.h"
+void User :: setId ( int newId ){
+    id=newId;
+}
+void User :: setLogin ( string newLogin ){
+    login=newLogin;
+}
+void User :: setName ( string newName ){
+    name=newName;
+}
+void User :: setSurname ( string newSurname ){
+    surname = newSurname;
+}
+void User :: setPassword ( string newPassword ){
+    password = newPassword;
+
+}
